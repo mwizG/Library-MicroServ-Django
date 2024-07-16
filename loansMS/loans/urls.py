965f76a18e1a2d77b25d652d11af013b27b10969
mwiza <mwiza0979001175@gmail.com>
2024-07-16 12:47:53 +0200

    rehalled the my books logic

--- loansMS/loans/urls.py
+++ loansMS/loans/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . views import ReturnBookView
-from .views import create_loan,borrow_book,my_books_view
+from .views import create_loan,borrow_book,MyBooksView
 
 app_name='loans'
 urlpatterns = [
     path('return/', ReturnBookView.as_view(),name='return'),
     path('create/', create_loan, name='create'),
     path('borrow/', borrow_book, name='borrow'),
-    path('mybookapi/', my_books_view, name='mybooks'),
+    path('mybookapi/', MyBooksView.as_view(), name='mybooks'),
     
 ]
