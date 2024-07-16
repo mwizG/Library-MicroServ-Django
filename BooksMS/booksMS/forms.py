be23c87579cebdf73e8c56f6fda312840fc4be4c
mwiza <mwiza0979001175@gmail.com>
2024-07-16 16:07:29 +0200

    changed the books form

--- BooksMS/booksMS/forms.py
+++ BooksMS/booksMS/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 
 class BookForm(forms.ModelForm):
+    # Define form fields with specific types and attributes
+    pub_year = forms.DateField(label="Date published", input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}))
+    coverimg = forms.CharField(max_length=200, required=False)
+    rating = forms.IntegerField(required=False)
+    genre = forms.CharField(max_length=200, required=False)
+    author = forms.ModelChoiceField(queryset=Authors.objects.all(), required=False, empty_label="---------")
+
     class Meta:
         model = Books
         fields = '__all__'
